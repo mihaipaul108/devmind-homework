b7ac8e57806c55b5b1ac549b8d741181324bced4
mihaipaul108 <mihai3010@gmail.com>
2024-05-12 20:04:36 +0300

    exercitiul 3

---
+++ skel/src/main/Exercise.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exercise {
+
+  public void solve(Scanner scanner, BufferedWriter writer) {
+    // TO DO:
+  }
+
+  private static class WordStats {
+    // TO DO:
+  }
+}
